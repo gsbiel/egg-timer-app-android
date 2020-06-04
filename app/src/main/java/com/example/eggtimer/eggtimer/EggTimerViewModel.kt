4c3349d7d865fb3612626fd71a42b86fcf7b3eb1
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-06-04 10:04:08 -0300

    implement CountDownTimer

--- app/src/main/java/com/example/eggtimer/eggtimer/EggTimerViewModel.kt
+++ app/src/main/java/com/example/eggtimer/eggtimer/EggTimerViewModel.kt
@@ -1,32 +1,72 @@
 package com.example.eggtimer.eggtimer
 
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlin.math.roundToInt
 
+class EggTimerViewModel: ViewModel(), MyTimerDelegate {
 
-class EggTimerViewModel: ViewModel() {
+    val SOFT_TIME: Long = 10000
+    val MEDIUM_TIME: Long = 20000
+    val HARD_TIME: Long = 30000
 
-    val SOFT_TIME: Double = 10.0
-    val MEDIUM_TIME: Double = 20.0
-    val HARD_TIME: Double = 30.0
-
-    private var _currentProgress = MutableLiveData<Double>()
-    val currentProgress: LiveData<Double>
+    private var _currentProgress = MutableLiveData<Int>()
+    val currentProgress: LiveData<Int>
         get() = _currentProgress
 
-    private val _currentMode: Double? = null
+    private var currentMode: Long? = null
+    private var timer: MyTimer? = null
 
     init {
-        _currentProgress.value = 0.0
+        _currentProgress.value = 0
     }
 
-    fun onButtonPressed(mode: Double = SOFT_TIME){
-        startTimerFor(mode)
+    fun onButtonPressed(mode: Long = SOFT_TIME){
+        timer?.let{
+            it.cancel()
+        }
+        currentMode = mode
+        setTimerFor(mode)
     }
 
-    private fun startTimerFor(time: Double) {
-        Log.i("EggTimerViewModel", "Starting timer for $time seconds")
+    private fun setTimerFor(time: Long){
+        timer = MyTimer(this, time)
+        timer?.start()
     }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.let{
+            it.cancel()
+        }
+        timer = null
+    }
+
+    override fun onTimerTick(millisUntilFinished: Long, total: Long) {
+        Log.i("MyTimer","Timer is counting: ${(millisUntilFinished/1000.0).roundToInt()} / ${total/1000}")
+        val progress: Int = (((millisUntilFinished/1000.0)/(total/1000.0))*100).roundToInt()
+        _currentProgress.value = progress
+    }
+
+    override fun onTimerFinished() {
+        Log.i("MyTimer", "Timer completed!")
+        _currentProgress.value = 0
+    }
+}
+
+private class MyTimer(val delegate: MyTimerDelegate, val total: Long, tick: Long = 1000): CountDownTimer(total, tick) {
+    override fun onFinish() {
+        delegate.onTimerFinished()
+    }
+    override fun onTick(millisUntilFinished: Long) {
+        delegate.onTimerTick(millisUntilFinished, total)
+    }
+}
+
+private interface MyTimerDelegate {
+    fun onTimerTick(millisUntilFinished: Long, total: Long)
+    fun onTimerFinished()
 }
